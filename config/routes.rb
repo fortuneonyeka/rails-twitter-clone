d059b081b57207394f2435677406664196298dcd
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-01-17 10:08:03 +0100

    refactor README Docs

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # devise_for :users, :controllers => { registrations: 'registrations' }
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :tweets
+  # post '/users/:id/follow', to: "users#follow", as: "follow_user"
+  # post '/users/:id/unfollow', to: "users#unfollow", as: "unfollow_user"
 
 
   root "tweets#index"
